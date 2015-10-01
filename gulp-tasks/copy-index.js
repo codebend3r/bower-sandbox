c8f458f2915b49133098c9de199be2ab95133279
Chester Rivas <chester.rivas@gmail.com>
2015-10-01 13:16:29 -0400

    unignoring build folder

--- gulp-tasks/copy-index.js
+++ gulp-tasks/copy-index.js
@@ -5,7 +5,8 @@ module.exports = function (gulp, plugins, config, gutil) {
   return function () {
 
     return gulp.src([
-        config.app + '/index.html'
+        config.app + '/index.html',
+        config.app + '/index-absolute.html'
       ])
       .pipe(gulp.dest(config.buildFolder))
       .on('error', gutil.log);
